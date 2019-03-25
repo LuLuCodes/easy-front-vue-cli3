0f3903fed95a36f32c2f16241d9c2b7e4497f775
qianqing <qian.qing@aliyun.com>
2019-03-25 12:53:10 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -96,5 +96,12 @@ module.exports = {
       }
       // #endregion 分析打包体积
     }
+  },
+  devServer: {
+    port: 8080, // 端口号
+    host: 'localhost',
+    https: false, // https:{type:Boolean}
+    open: true // 配置自动启动浏览器
+    // proxy: 'http://localhost:4000' // 配置跨域处理,只有一个代理
   }
 }
