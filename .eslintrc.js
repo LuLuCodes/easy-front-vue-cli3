ada1da02f866d64649db176db857b565df7f2d1c
qianqing <qian.qing@aliyun.com>
2019-04-27 17:40:29 +0800

    fix bug

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,8 +8,8 @@ module.exports = {
     '@vue/standard'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'no-console': 'off',
+    'no-debugger': 'off',
     'arrow-parens': 'off',
     'generator-star-spacing': 'off',
     'semi': ['error', 'always'],
@@ -23,4 +23,4 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   }
-}
+};
