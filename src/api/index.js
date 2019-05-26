50b677730376a006e81ae59a72d68c8c4396b121
qianqing <qian.qing@aliyun.com>
2019-05-26 17:35:34 +0800

    fix bug

--- src/api/index.js
+++ src/api/index.js
@@ -35,7 +35,7 @@ service.interceptors.response.use(response => {
   // 如果返回的状态码为200，说明接口请求成功，可以正常拿到数据
   // 否则的话抛出错误
   if (responseCode === 200) {
-    if (response.data.Data || response.data.token) {
+    if (response.data.Data && response.data.Data.authToken) {
       window.localStorage.setItem('authToken', response.data.Data.authToken);
     }
     return Promise.resolve(response.data);
