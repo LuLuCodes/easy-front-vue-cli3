ad1479e64e7eedc31919740ad2e524e7a0407af4
qianqing <qian.qing@aliyun.com>
2019-06-29 20:53:30 +0800

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -36,20 +36,20 @@ module.exports = {
 
     config.output.filename('[name].[hash].js').end();
     // #region svg-config
-    const svgRule = config.module.rule('svg'); // 找到svg-loader
-    svgRule.uses.clear(); // 清除已有的loader, 如果不这样做会添加在此loader之后
-    svgRule.exclude.add(/node_modules/); // 正则匹配排除node_modules目录
-    svgRule // 添加svg新的loader处理
-      .test(/\.svg$/)
-      .use('svg-sprite-loader')
-      .loader('svg-sprite-loader')
-      .options({
-        symbolId: 'icon-[name]'
-      });
+    // const svgRule = config.module.rule('svg'); // 找到svg-loader
+    // svgRule.uses.clear(); // 清除已有的loader, 如果不这样做会添加在此loader之后
+    // svgRule.exclude.add(/node_modules/); // 正则匹配排除node_modules目录
+    // svgRule // 添加svg新的loader处理
+    //   .test(/\.svg$/)
+    //   .use('svg-sprite-loader')
+    //   .loader('svg-sprite-loader')
+    //   .options({
+    //     symbolId: 'icon-[name]'
+    //   });
 
-    // 修改images loader 添加svg处理
-    const imagesRule = config.module.rule('images');
-    imagesRule.exclude.add(path.resolve('src/assets/icons'));
+    // // 修改images loader 添加svg处理
+    // const imagesRule = config.module.rule('images');
+    // imagesRule.exclude.add(path.resolve('src/assets/icons'));
     // #endregion svg-config
 
 
