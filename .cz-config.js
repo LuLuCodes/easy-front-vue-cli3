8e3320666b3e73c086d89d2d4f1b2687214136a1
e <leyi@myun.info>
2019-11-27 16:25:57 +0800

    style(cz-config): cz-config格式问题

--- .cz-config.js
+++ .cz-config.js
@@ -3,43 +3,48 @@ module.exports = {
   types: [
     {
       value: "feat",
-      name: "✨  feat:     A new feature"
+      name: "feat:     一个新功能"
     },
     {
       value: "fix",
-      name: "🐞  fix:      A bug fix"
+      name: "fix:      一个 bug 修复"
     },
     {
       value: "refactor",
-      name:
-        "🛠  refactor: A code change that neither fixes a bug nor adds a feature"
+      name: "refactor: 既不是修复 bug 也不是添加新功能的代码变更"
     },
     {
       value: "docs",
-      name: "📚  docs:     Documentation only changes"
+      name: "docs:     只有文档发生变更"
     },
     {
       value: "test",
-      name: "🏁  test:     Add missing tests or correcting existing tests"
+      name: "test:     添加或者修改测试脚本"
     },
     {
       value: "chore",
-      name:
-        "🗯  chore:    Changes that don't modify src or test files. Such as updating build tasks, package manager"
+      name: "chore:   影响构建系统或外部依赖的更改（例如：gulp，npm，webpack）"
     },
     {
       value: "style",
-      name:
-        "💅  style:    Code Style, Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)"
+      name: "style:    对代码逻辑无影响的变更 (空格, 格式化, 缺少分号等)"
     },
     {
       value: "revert",
-      name: "⏪  revert:   Revert to a commit"
+      name: "revert:   撤销上一次的提交"
     }
   ],
 
   scopes: [],
-
+  messages: {
+    type: "选择你提交的变更类型:",
+    customScope: "(非必填)变更所影响的范围:",
+    subject: "(必填)变更的简短描述:\n",
+    body: "(非必填)变更的详细描述:\n",
+    breaking: "(非必填)非兼容性变更说明:\n",
+    footer: "(非必填)变更所关联的Issue ID， 例如: #31, #34:\n",
+    confirmCommit: "你确定要继续执行上面的提交吗？"
+  },
   allowCustomScopes: true,
   allowBreakingChanges: ["feat", "fix"]
 };
