59606a4f508ebfc7238b4a684116260fb40ca885
e <leyi@myun.info>
2019-12-07 11:56:50 +0800

    refactor: 增加备注

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,9 +9,10 @@ workbox.core.setCacheNameDetails({
   prefix: 'easy-front-vue-cli3',
   suffix: 'v1.0.0'
 });
+
 // have our sw update and control a web page as soon as possible.
-workbox.core.skipWaiting();
-workbox.core.clientsClaim();
+workbox.core.skipWaiting(); // 强制等待中的 Service Worker 被激活
+workbox.core.clientsClaim(); // Service Worker 被激活后使其立即获得页面控制权
 
 // vue-cli3.0 supports pwa with the help of workbox-webpack-plugin, we need to get the precacheing list through this sentence.
 workbox.precaching.precacheAndRoute(self.__precacheManifest || []);
@@ -48,27 +49,40 @@ workbox.routing.registerRoute(
   })
 );
 
-// 获取oss上的资源，支持跨域
-// workbox.routing.registerRoute(
-//   /^https:\/\/images\.lancky\.com\/.*\.(jpe?g|png|gif|svg)/,
-//   workbox.strategies.staleWhileRevalidate({
-//     cacheName: 'oss-images',
-//     plugins: [
-//       new workbox.expiration.Plugin({
-//         maxEntries: 60,
-//         maxAgeSeconds: 5 * 24 * 60 * 60 // 5 Days
-//       })
-//     ],
-//     fetchOptions: {
-//       credentials: 'include'
-//     }
-//   })
-// );
+// 获取cdn上的资源，支持跨域
+workbox.routing.registerRoute(
+  /^https:\/\/images\.lancky\.com\/.*\.(jpe?g|png|gif|svg)/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'cdn-images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 5 * 24 * 60 * 60 // 5 Days
+      })
+    ],
+    fetchOptions: {
+      credentials: 'include'
+    }
+  })
+);
 
-// api缓存，优选从网络获取，网络异常时再使用缓存，请根据实际api url配置RegExp
+// api缓存，优选从网络获取，网络异常时再使用缓存，请根据实际api url配置RegExp，只支持get请求
 workbox.routing.registerRoute(
   new RegExp('https://m.hellomrbigbigshot.xyz/api'),
   workbox.strategies.networkFirst({
     cacheName: 'api'
   })
 );
+
+// api缓存，优选从网络获取，网络异常时再使用缓存，请根据实际api url配置RegExp，支持post请求
+// const bgSyncPlugin = new workbox.backgroundSync.Plugin('apiQueue', {
+//   maxRetentionTime: 1 * 60
+// });
+
+// workbox.routing.registerRoute(
+//   /.*\/api\/.*/,
+//   new workbox.strategies.NetworkOnly({
+//     plugins: [bgSyncPlugin]
+//   }),
+//   'POST'
+// );
