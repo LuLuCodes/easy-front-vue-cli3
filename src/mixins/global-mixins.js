a5e76940f64452fcb5ee5dfec74031b7167d1983
mac <mac@macdeMac-7.local>
2020-05-14 17:37:44 +0800

    add post

--- src/mixins/global-mixins.js
+++ src/mixins/global-mixins.js
@@ -1,50 +1,107 @@
 export default {
   data() {
     return {
-      dataLoading: false
+      pending: new Map()
     };
   },
   methods: {
-    load({
+    async post({
+      url = '',
+      data = {},
       message = '加载中',
       overlay = false,
       forbidClick = true,
       duration = 0
     } = {}) {
-      if (this.dataLoading) {
-        return this.dataLoading;
+      if (!url && typeof url === 'string') {
+        console.error('need a string url!');
+        return { ok: false, data: null };
       }
-      this.dataLoading = true;
-      this.$toast({
-        type: 'loading',
-        message,
-        overlay,
-        forbidClick,
-        duration,
-        icon: require('../assets/images/common/loading-white-1.png'),
-        className: 'toast-white'
-      });
-      return false;
+
+      const prams = [url, JSON.stringify(data)].join('&');
+      try {
+        if (!this.pending.has(prams)) {
+          // 如果 pending 中不存在当前请求，则添加进去
+          this.pending.set(prams, true);
+          this.$toast({
+            type: 'loading',
+            message,
+            overlay,
+            forbidClick,
+            duration,
+            icon: require('../assets/images/common/loading-white-1.png'),
+            className: 'toast-white'
+          });
+          const result = await this.$api.post({ url, data });
+          this.pending.delete(prams);
+          this.$toast.clear();
+          return { ok: true, data: result };
+        } else {
+          console.error('this url request is wating response!');
+          return { ok: false, data: null };
+        }
+      } catch (error) {
+        this.$toast.clear();
+        this.$toast({
+          type: 'fail',
+          message,
+          overlay,
+          forbidClick,
+          duration
+        });
+        this.pending.delete(prams);
+        return { ok: false, data: null };
+      }
     },
-    unload() {
-      this.dataLoading = false;
-      this.$toast.clear();
-    },
-    errorMsg({
-      message = '网络异常，请稍后再试！',
+    async get({
+      url = '',
+      data = {},
+      message = '加载中',
       overlay = false,
       forbidClick = true,
-      duration = 3000
+      duration = 0
     } = {}) {
-      this.dataLoading = false;
-      this.$toast.clear();
-      this.$toast({
-        type: 'fail',
-        message,
-        overlay,
-        forbidClick,
-        duration
-      });
+      if (!url && typeof url === 'string') {
+        console.error('need a string url!');
+        return { ok: false, data: null };
+      }
+      const prams = [url, JSON.stringify(data)].join('&');
+      try {
+        if (!this.pending.has(prams)) {
+          // 如果 pending 中不存在当前请求，则添加进去
+          this.pending.set(prams, true);
+          this.$toast({
+            type: 'loading',
+            message,
+            overlay,
+            forbidClick,
+            duration,
+            icon: require('../assets/images/common/loading-white-1.png'),
+            className: 'toast-white'
+          });
+          const result = await this.$api.post({ url, data });
+          this.pending.delete(prams);
+          this.$toast.clear();
+          return { ok: true, data: result };
+        } else {
+          console.error('this url request is wating response!');
+          return { ok: false, data: null };
+        }
+      } catch (error) {
+        this.$toast.clear();
+        this.$toast({
+          type: 'fail',
+          message,
+          overlay,
+          forbidClick,
+          duration
+        });
+        this.pending.delete(prams);
+        return { ok: false, data: null };
+      }
+    },
+    goBack() {
+      this.$router.go(-1);
     }
   }
 };
