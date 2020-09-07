55e975127c77442ec5efc805cde6693e4c0741c8
mac <mac@macdeMac-7.local>
2020-09-07 21:08:03 +0800

    恢复被删除的脚本

---
+++ scripts/template.js
@@ -0,0 +1,50 @@
+module.exports = {
+  vueTemplate: componentName => {
+    return `<template>
+  <div class="${componentName}">
+    ${componentName}组件
+  </div>
+</template>
+<script>
+export default {
+  name: '${componentName}',
+  components: {},
+  data() {
+    return {};
+  },
+  props: {
+    prop: {
+      type: Object,
+      default() {
+        return {};
+      }
+    }
+  },
+  computed: {},
+  created() {
+    // api请求尽量放在这里
+    // TODO
+  },
+  mounted() {
+    // 对数据的操作尽量放在这里
+    // TODO
+  },
+  updated() {
+    // TODO
+  },
+  beforeDestroy() {
+    // TODO
+  },
+  watch: {},
+  methods: {}
+};
+</script>
+<style lang="less" scoped>
+.${componentName} {
+}
+</style>
+`;
+  },
+  entryTemplate: `import Main from './main.vue';
+export default Main;`
+};
